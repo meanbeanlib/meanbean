c1be42c312ea323c7474ff8d6bf768d4a28560c3
Graham Williamson <graham@meanbean.org>
2011-02-12 15:24:48 +0000

    3179054 - Formatted javadoc
    git-svn-id: https://svn.code.sf.net/p/meanbean/code/meanbean@48 ef271720-08f6-4cbf-8bd4-1a591c3b7128

--- src/main/java/org/meanbean/factories/FactoryCollection.java
+++ src/main/java/org/meanbean/factories/FactoryCollection.java
@@ -10,10 +10,14 @@ import org.meanbean.lang.Factory;
 public interface FactoryCollection {
 
 	/**
+	 * <p>
 	 * Add the specified Factory to the collection.
+	 * </p>
 	 * 
+	 * <p>
 	 * If a Factory is already registered against the specified class, the existing registered Factory will be replaced
 	 * with the Factory you specify here.
+	 * </p>
 	 * 
 	 * @param clazz
 	 *            The type of objects the Factory creates. The class type will be used to generate a key with which the
@@ -27,10 +31,14 @@ public interface FactoryCollection {
 	void addFactory(Class<?> clazz, Factory<?> factory) throws IllegalArgumentException;
 
 	/**
+	 * <p>
 	 * Get the Factory registered for the specified class.
+	 * </p>
 	 * 
+	 * <p>
 	 * To check whether a Factory is registered for a specified class, please refer to
 	 * <code>hasFactory(Class<?> clazz);</code>.
+	 * </p>
 	 * 
 	 * @param clazz
 	 *            The class the Factory is registered against. This should be the type of object that the Factory
