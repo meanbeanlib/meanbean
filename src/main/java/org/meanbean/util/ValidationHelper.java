23ace4f3f98a3671be4a6ddf94a83461e5186288
Graham Williamson <graham@meanbean.org>
2011-02-12 15:28:53 +0000

    3179054 - Formatted javadoc
    git-svn-id: https://svn.code.sf.net/p/meanbean/code/meanbean@51 ef271720-08f6-4cbf-8bd4-1a591c3b7128

--- src/main/java/org/meanbean/util/ValidationHelper.java
+++ src/main/java/org/meanbean/util/ValidationHelper.java
@@ -7,37 +7,44 @@ package org.meanbean.util;
  */
 public interface ValidationHelper {
 
-    /**
-     * Ensure that the specified value exists, conditionally throwing an IllegalArgumentException if it does not. <br/>
-     * 
-     * The exception thrown will contain the name of the parameter that must exist.
-     * 
-     * @param name
-     *            The name of the object that must exist.
-     * @param value
-     *            The object that must exist.
-     * 
-     * @throws IllegalArgumentException
-     *             If the specified value does not exist.
-     */
-    void ensureExists(String name, Object value) throws IllegalArgumentException;
+	/**
+	 * <p>
+	 * Ensure that the specified value exists, conditionally throwing an IllegalArgumentException if it does not. <br/>
+	 * </p>
+	 * 
+	 * <p>
+	 * The exception thrown will contain the name of the parameter that must exist.
+	 * </p>
+	 * 
+	 * @param name
+	 *            The name of the object that must exist.
+	 * @param value
+	 *            The object that must exist.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If the specified value does not exist.
+	 */
+	void ensureExists(String name, Object value) throws IllegalArgumentException;
 
-    /**
-     * Ensure that the specified value exists, conditionally throwing an IllegalArgumentException if it does not. <br/>
-     * 
-     * The exception thrown will contain the name of the parameter that must exist as well as the operation being
-     * attempted.
-     * 
-     * @param name
-     *            The name of the object that must exist.
-     * @param operation
-     *            The operation being attempted.
-     * @param value
-     *            The object that must exist.
-     * 
-     * @throws IllegalArgumentException
-     *             If the specified value does not exist.
-     */
-    void ensureExists(String name, String operation, Object value) throws IllegalArgumentException;
-
+	/**
+	 * <p>
+	 * Ensure that the specified value exists, conditionally throwing an IllegalArgumentException if it does not. <br/>
+	 * </p>
+	 * 
+	 * <p>
+	 * The exception thrown will contain the name of the parameter that must exist as well as the operation being
+	 * attempted.
+	 * </p>
+	 * 
+	 * @param name
+	 *            The name of the object that must exist.
+	 * @param operation
+	 *            The operation being attempted.
+	 * @param value
+	 *            The object that must exist.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If the specified value does not exist.
+	 */
+	void ensureExists(String name, String operation, Object value) throws IllegalArgumentException;
 }
