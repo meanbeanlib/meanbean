f46a7859b1afcd7f2b5312b0ed47a95a9125c2b7
Graham Williamson <graham@meanbean.org>
2011-06-18 15:00:00 +0000

    Correct typo in LongFactory comment which mentioned Integer rather than Long.
    git-svn-id: https://svn.code.sf.net/p/meanbean/code/trunk@127 ef271720-08f6-4cbf-8bd4-1a591c3b7128

--- src/main/java/org/meanbean/factories/basic/LongFactory.java
+++ src/main/java/org/meanbean/factories/basic/LongFactory.java
@@ -9,29 +9,29 @@ import org.meanbean.util.RandomValueGenerator;
  */
 public final class LongFactory extends RandomFactoryBase<Long> {
 
-    /** Unique version ID of this Serializable class. */
-    private static final long serialVersionUID = 1L;
-    
-    /**
-     * Construct a new Integer object factory.
-     * 
-     * @param randomValueGenerator
-     *            A random value generator used by the Factory to generate random values.
-     *            
-     * @throws IllegalArgumentException
-     *             If the specified randomValueGenerator is deemed illegal. For example, if it is null.
-     */
-    public LongFactory(RandomValueGenerator randomValueGenerator) throws IllegalArgumentException {
-        super(randomValueGenerator);
-    }
+	/** Unique version ID of this Serializable class. */
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Create a new Long object.
-     * 
-     * @return A new Long object.
-     */
-    @Override
-    public Long create() {
-        return getRandomValueGenerator().nextLong();
-    }
+	/**
+	 * Construct a new Long object factory.
+	 * 
+	 * @param randomValueGenerator
+	 *            A random value generator used by the Factory to generate random values.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If the specified randomValueGenerator is deemed illegal. For example, if it is null.
+	 */
+	public LongFactory(RandomValueGenerator randomValueGenerator) throws IllegalArgumentException {
+		super(randomValueGenerator);
+	}
+
+	/**
+	 * Create a new Long object.
+	 * 
+	 * @return A new Long object.
+	 */
+	@Override
+	public Long create() {
+		return getRandomValueGenerator().nextLong();
+	}
 }
