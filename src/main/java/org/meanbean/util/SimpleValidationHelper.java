583e22dc4568ab13fb969497df0681ed3067c366
Graham Williamson <graham@meanbean.org>
2012-07-14 12:43:19 +0000

    Cleaned tests.
    git-svn-id: https://svn.code.sf.net/p/meanbean/code/trunk@173 ef271720-08f6-4cbf-8bd4-1a591c3b7128

--- src/main/java/org/meanbean/util/SimpleValidationHelper.java
+++ src/main/java/org/meanbean/util/SimpleValidationHelper.java
@@ -11,93 +11,93 @@ import org.apache.commons.logging.Log;
  */
 public class SimpleValidationHelper implements ValidationHelper, Serializable {
 
-	/** Unique version ID of this Serializable class. */
-	private static final long serialVersionUID = 1L;
+    /** Unique version ID of this Serializable class. */
+    private static final long serialVersionUID = 1L;
 
-	/** Logging mechanism. */
-	private final transient Log log;
+    /** Logging mechanism. */
+    private final transient Log log;
 
-	/**
-	 * Construct a new Simple Validation Helper.
-	 */
-	public SimpleValidationHelper() {
-		this(null);
-	}
+    /**
+     * Construct a new Simple Validation Helper.
+     */
+    public SimpleValidationHelper() {
+        this(null);
+    }
 
-	/**
-	 * Construct a new Simple Validation Helper.
-	 * 
-	 * @param log
-	 *            Logging mechanism to use when logging validation exceptions.
-	 */
-	public SimpleValidationHelper(Log log) {
-		this.log = log;
-	}
+    /**
+     * Construct a new Simple Validation Helper.
+     * 
+     * @param log
+     *            Logging mechanism to use when logging validation exceptions.
+     */
+    public SimpleValidationHelper(Log log) {
+        this.log = log;
+    }
 
-	/**
-	 * <p>
-	 * Ensure that the specified value exists, conditionally throwing an IllegalArgumentException if it does not. <br/>
-	 * </p>
-	 * 
-	 * <p>
-	 * The exception thrown will contain the name of the parameter that must exist.
-	 * </p>
-	 * 
-	 * @param name
-	 *            The name of the object that must exist.
-	 * @param value
-	 *            The object that must exist.
-	 * 
-	 * @throws IllegalArgumentException
-	 *             If the specified value does not exist.
-	 */
-	public void ensureExists(String name, Object value) throws IllegalArgumentException {
-		log("ensureExists: entering with name=[" + name + "], value=[" + value + "].");
-		if (value == null) {
-			log("ensureExists: Object [" + name + "] does not exist. Throw IllegalArgumentException..");
-			throw new IllegalArgumentException("Object [" + name + "] must be provided.");
-		}
-		log("ensureExists: exiting.");
-	}
+    /**
+     * <p>
+     * Ensure that the specified value exists, conditionally throwing an IllegalArgumentException if it does not. <br/>
+     * </p>
+     * 
+     * <p>
+     * The exception thrown will contain the name of the parameter that must exist.
+     * </p>
+     * 
+     * @param name
+     *            The name of the object that must exist.
+     * @param value
+     *            The object that must exist.
+     * 
+     * @throws IllegalArgumentException
+     *             If the specified value does not exist.
+     */
+    public void ensureExists(String name, Object value) throws IllegalArgumentException {
+        log("ensureExists: entering with name=[" + name + "], value=[" + value + "].");
+        if (value == null) {
+            log("ensureExists: Object [" + name + "] does not exist. Throw IllegalArgumentException.");
+            throw new IllegalArgumentException("Object [" + name + "] must be provided.");
+        }
+        log("ensureExists: exiting.");
+    }
 
-	/**
-	 * <p>
-	 * Ensure that the specified value exists, conditionally throwing an IllegalArgumentException if it does not. <br/>
-	 * </p>
-	 * 
-	 * <p>
-	 * The exception thrown will contain the name of the parameter that must exist as well as the operation being
-	 * attempted.
-	 * </p>
-	 * 
-	 * @param name
-	 *            The name of the object that must exist.
-	 * @param operation
-	 *            The operation being attempted.
-	 * @param value
-	 *            The object that must exist.
-	 * 
-	 * @throws IllegalArgumentException
-	 *             If the specified value does not exist.
-	 */
-	public void ensureExists(String name, String operation, Object value) throws IllegalArgumentException {
-		log("ensureExists: entering with name=[" + name + "], operation=[" + operation + "], value=[" + value + "].");
-		if (value == null) {
-			log("ensureExists: Object [" + name + "] does not exist. Throw IllegalArgumentException..");
-			throw new IllegalArgumentException("Cannot " + operation + " with null " + name + ".");
-		}
-		log("ensureExists: exiting.");
-	}
+    /**
+     * <p>
+     * Ensure that the specified value exists, conditionally throwing an IllegalArgumentException if it does not. <br/>
+     * </p>
+     * 
+     * <p>
+     * The exception thrown will contain the name of the parameter that must exist as well as the operation being
+     * attempted.
+     * </p>
+     * 
+     * @param name
+     *            The name of the object that must exist.
+     * @param operation
+     *            The operation being attempted.
+     * @param value
+     *            The object that must exist.
+     * 
+     * @throws IllegalArgumentException
+     *             If the specified value does not exist.
+     */
+    public void ensureExists(String name, String operation, Object value) throws IllegalArgumentException {
+        log("ensureExists: entering with name=[" + name + "], operation=[" + operation + "], value=[" + value + "].");
+        if (value == null) {
+            log("ensureExists: Object [" + name + "] does not exist. Throw IllegalArgumentException..");
+            throw new IllegalArgumentException("Cannot " + operation + " with null " + name + ".");
+        }
+        log("ensureExists: exiting.");
+    }
 
-	/**
-	 * Log the specified message if logging is configured in this Validation Helper.
-	 * 
-	 * @param message
-	 *            The message to log.
-	 */
-	private void log(String message) {
-		if (log != null) {
-			log.debug(message);
-		}
-	}
+    /**
+     * Log the specified message if logging is configured in this Validation Helper.
+     * 
+     * @param message
+     *            The message to log.
+     */
+    private void log(String message) {
+        if (log != null) {
+            log.debug(message);
+        }
+    }
 }
--- src/test/java/org/meanbean/util/SimpleValidationHelperTest.java
+++ src/test/java/org/meanbean/util/SimpleValidationHelperTest.java
@@ -1,48 +1,72 @@
 package org.meanbean.util;
 
-import org.apache.commons.logging.Log;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
-@RunWith(MockitoJUnitRunner.class)
+import org.junit.Test;
+
 public class SimpleValidationHelperTest {
 
-	private static final String NAME = "TEST_NAME";
+    private static final String NAME = "TEST_NAME";
 
-	private static final String OPERATION = "TEST_OPERATION";
+    private static final String OPERATION = "TEST_OPERATION";
 
-	private static final Object VALID_VALUE = "TEST_VALUE";
+    private static final Object VALID_VALUE = "TEST_VALUE";
 
-	@Mock
-	private Log log;
+    private static final Object NULL_VALUE = null;
 
-	private SimpleValidationHelper simpleValidationHelper;
+    private final SimpleValidationHelper simpleValidationHelper = new SimpleValidationHelper();
 
-	@Before
-	public void before() {
-		simpleValidationHelper = new SimpleValidationHelper(log);
-	}
+    @Test
+    public void shouldPreventInvalidValueWhenNameAndValueProvided() {
+        // Given
+        Exception expectedException = new IllegalArgumentException("Object [" + NAME + "] must be provided.");
+        // When
+        Exception actualException = null;
+        try {
+            simpleValidationHelper.ensureExists(NAME, NULL_VALUE);
+        } catch (Exception exception) {
+            actualException = exception;
+        }
+        // Then
+        assertIsExpectedException(expectedException, actualException);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void ensureExistsWithInvalidNameAndValue() {
-		simpleValidationHelper.ensureExists(NAME, null);
-	}
+    @Test
+    public void shouldPreventInvalidValueWhenNameAndOperationAndValueProvided() {
+        // Given
+        Exception expectedException = new IllegalArgumentException("Cannot " + OPERATION + " with null " + NAME + ".");
+        // When
+        Exception actualException = null;
+        try {
+            simpleValidationHelper.ensureExists(NAME, OPERATION, NULL_VALUE);
+        } catch (Exception exception) {
+            actualException = exception;
+        }
+        // Then
+        assertIsExpectedException(expectedException, actualException);
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void ensureExistsWithInvalidNameAndValueAndOperation() {
-		simpleValidationHelper.ensureExists(NAME, OPERATION, null);
-	}
+    private void assertIsExpectedException(Exception expectedException, Exception actualException) {
+        assertThat("Expected exception not thrown.", actualException, is(instanceOf(expectedException.getClass())));
+        assertThat("Expected exception message not present.", actualException.getMessage(),
+                is(expectedException.getMessage()));
+    }
 
-	@Test
-	public void ensureExistsWithValidNameAndValue() {
-		simpleValidationHelper.ensureExists(NAME, VALID_VALUE);
-	}
+    @Test
+    public void ensureExistsWithValidNameAndValue() {
+        // Given
+        // When
+        simpleValidationHelper.ensureExists(NAME, VALID_VALUE);
+        // Then - no Exception is thrown
+    }
 
-	@Test
-	public void ensureExistsWithValidNameAndValueAndOperation() {
-		simpleValidationHelper.ensureExists(NAME, OPERATION, VALID_VALUE);
-	}
+    @Test
+    public void ensureExistsWithValidNameAndValueAndOperation() {
+        // Given
+        // When
+        simpleValidationHelper.ensureExists(NAME, OPERATION, VALID_VALUE);
+        // Then - no Exception is thrown
+    }
 }
