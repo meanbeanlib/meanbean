3d86b7f47ed1ad83faa6bf7e4bdb3a374c73f813
Graham Williamson <graham@meanbean.org>
2012-07-14 06:39:52 +0000

    Cleaned tests.
    git-svn-id: https://svn.code.sf.net/p/meanbean/code/trunk@170 ef271720-08f6-4cbf-8bd4-1a591c3b7128

--- src/test/java/org/meanbean/bean/info/PropertyInformationFactory.java
+++ src/test/java/org/meanbean/bean/info/PropertyInformationFactory.java
@@ -3,59 +3,59 @@ package org.meanbean.bean.info;
 import java.lang.reflect.Method;
 
 /**
- * Factory that creates PropertyInformation objects.
+ * Factory that creates PropertyInformation objects. Should be used for testing only.
  * 
  * @author Graham Williamson
  */
 public class PropertyInformationFactory {
 
-	/**
-	 * Create a new PropertyInformation object with the specified parameters. The new PropertyInformation will not have
-	 * a read method nor write method.
-	 * 
-	 * @param name
-	 *            The name of the property.
-	 * @param isReadable
-	 *            <code>true</code> if the property is readable, <code>false</code> if it is not readable.
-	 * @param isWritable
-	 *            <code>true</code> if the property is writable, <code>false</code> if it is not writable.
-	 * 
-	 * @return A new PropertyInformation object.
-	 */
-	public static PropertyInformation create(String name, boolean isReadable, boolean isWritable) {
-		return create(name, isReadable, isWritable, null, null, null, null);
-	}
+    /**
+     * Create a new PropertyInformation object with the specified parameters. The new PropertyInformation will not have
+     * a read method nor write method.
+     * 
+     * @param name
+     *            The name of the property.
+     * @param isReadable
+     *            <code>true</code> if the property is readable, <code>false</code> if it is not readable.
+     * @param isWritable
+     *            <code>true</code> if the property is writable, <code>false</code> if it is not writable.
+     * 
+     * @return A new PropertyInformation object.
+     */
+    public static PropertyInformation create(String name, boolean isReadable, boolean isWritable) {
+        return create(name, isReadable, isWritable, null, null, null, null);
+    }
 
-	/**
-	 * Create a new PropertyInformation object with the specified parameters.
-	 * 
-	 * @param name
-	 *            The name of the property.
-	 * @param isReadable
-	 *            <code>true</code> if the property is readable, <code>false</code> if it is not readable.
-	 * @param isWritable
-	 *            <code>true</code> if the property is writable, <code>false</code> if it is not writable.
-	 * @param readMethod
-	 *            The read method for the property, or <code>null</code> if the property has no read method.
-	 * @param writeMethod
-	 *            The write method for the property, or <code>null</code> if the property has no write method.
-	 * @param readMethodReturnType
-	 *            The return type of the read method.
-	 * @param writeMethodParameterType
-	 *            The parameter type of the write method.
-	 * 
-	 * @return A new PropertyInformation object.
-	 */
-	public static PropertyInformation create(String name, boolean isReadable, boolean isWritable, Method readMethod,
-	        Method writeMethod, Class<?> readMethodReturnType, Class<?> writeMethodParameterType) {
-		PropertyInformationBean propertyInformation = new PropertyInformationBean();
-		propertyInformation.setName(name);
-		propertyInformation.setReadable(isReadable);
-		propertyInformation.setWritable(isWritable);
-		propertyInformation.setReadMethod(readMethod);
-		propertyInformation.setWriteMethod(writeMethod);
-		propertyInformation.setReadMethodReturnType(readMethodReturnType);
-		propertyInformation.setWriteMethodParameterType(writeMethodParameterType);
-		return propertyInformation;
-	}
+    /**
+     * Create a new PropertyInformation object with the specified parameters.
+     * 
+     * @param name
+     *            The name of the property.
+     * @param isReadable
+     *            <code>true</code> if the property is readable, <code>false</code> if it is not readable.
+     * @param isWritable
+     *            <code>true</code> if the property is writable, <code>false</code> if it is not writable.
+     * @param readMethod
+     *            The read method for the property, or <code>null</code> if the property has no read method.
+     * @param writeMethod
+     *            The write method for the property, or <code>null</code> if the property has no write method.
+     * @param readMethodReturnType
+     *            The return type of the read method.
+     * @param writeMethodParameterType
+     *            The parameter type of the write method.
+     * 
+     * @return A new PropertyInformation object.
+     */
+    public static PropertyInformation create(String name, boolean isReadable, boolean isWritable, Method readMethod,
+            Method writeMethod, Class<?> readMethodReturnType, Class<?> writeMethodParameterType) {
+        PropertyInformationBean propertyInformation = new PropertyInformationBean();
+        propertyInformation.setName(name);
+        propertyInformation.setReadable(isReadable);
+        propertyInformation.setWritable(isWritable);
+        propertyInformation.setReadMethod(readMethod);
+        propertyInformation.setWriteMethod(writeMethod);
+        propertyInformation.setReadMethodReturnType(readMethodReturnType);
+        propertyInformation.setWriteMethodParameterType(writeMethodParameterType);
+        return propertyInformation;
+    }
 }
